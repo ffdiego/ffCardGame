490241e585535664e595386ccc262d6e309f95d1
Diego Fernandes <diego.silva@luz-ef.com>
2024-02-14 17:01:29 -0400

    ficou complexo

--- Base/Jogador.cs
+++ Base/Jogador.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +11,25 @@ namespace CardGame.Base
     {
         public string Nome;
         public List<Carta> Mao;
+        private WebSocket ws;
 
         public Jogador(string nome)
         {
             Nome = nome;
             Mao = new List<Carta>();
+        }
+
+        public async Task EnviaMensagem(string mensagem)
+        {
+            var buffer = new byte[1024 * 4];
+            await ws.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        public async Task<string> RecebeResposta()
+        {
+            var buffer = new byte[1024 * 4];
+            await ws.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+            return "oitutobom";
         }
     }
 }
--- Jogos/Bisca.cs
+++ Jogos/Bisca.cs
@@ -9,13 +9,72 @@ namespace CardGame.Jogos
 {
     public class Bisca
     {
+        // Definicoes fixas
+        readonly int MaximosJogadores = 6;
+        readonly int CartasNaMaoDeCadaJogador = 7;
+        readonly TimeSpan tempoPorRodada = TimeSpan.FromSeconds(15);
+
         public Baralho Baralho;
         public List<Jogador> Jogadores;
+        public bool Iniciou;
+        private Jogador JogadorAtual;
 
         public Bisca()
         {
             Baralho = new Baralho();
             Jogadores = new List<Jogador>();
+            Iniciou = false;
+        }
+
+        public bool AdicionaJogador(Jogador jogador)
+        {
+            if (Jogadores.Count > (MaximosJogadores + 1))
+            {
+                return false;
+            }
+
+            Jogadores.Add(jogador);
+            return true;
+        }
+
+        public bool IniciaJogo()
+        {
+            if (Jogadores.Count == 0)
+            {
+                return false;
+            }
+
+            foreach (var jogador in Jogadores)
+            {
+                jogador.Mao.AddRange(Baralho.PuxarCartas(CartasNaMaoDeCadaJogador));
+            }
+
+            Jogador jogadorAtual = Jogadores.FirstOrDefault()!;
+
+            return true;
+        }
+
+        public async void ExecutaRodada()
+        {
+            await JogadorAtual.EnviaMensagem("suavez");
+            
+            Task tempoEsgotado = Task.Delay(tempoPorRodada);
+            Task<string> respostaJogador = JogadorAtual.RecebeResposta();
+
+            var timeoutRodada = new CancellationTokenSource(tempoPorRodada);
+
+            Task terminouPrimeiro = await Task.WhenAny(tempoEsgotado, Task.Delay(tempoPorRodada));
+
+            if (terminouPrimeiro == respostaJogador)
+            {
+                Console.WriteLine($"Resposta do jogador: {respostaJogador.Result}");
+            } 
+            else
+            {
+                Console.WriteLine("O jogador demorou muito pra responder!");
+            }
+
+
         }
     }
 }
