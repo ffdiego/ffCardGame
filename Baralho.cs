eae71218e1f5ba11fb0e09031bf91c1e5e1d8d78
Diego Fernandes <diego.silva@luz-ef.com>
2024-02-14 12:03:03 -0400

    Initial commit

---
+++ Baralho.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    public class Baralho
+    {
+        public List<Carta> Cartas;
+        public List<Carta> CartasRemovidas;
+        public Baralho(bool comCoringa = false) 
+        {
+            int quantidadeNaipes = Enum.GetValues<Naipe>().Length;
+            int quantidadeNumeros = 13;
+            int quantidadeCoringas = comCoringa ? 2 : 0;
+
+            Cartas = new List<Carta>(quantidadeNaipes * quantidadeNumeros + quantidadeCoringas);
+            CartasRemovidas = new List<Carta>(quantidadeNaipes * quantidadeNumeros + quantidadeCoringas);
+            
+            foreach (var naipe in Enum.GetValues<Naipe>()) 
+            { 
+                for(int i = 1; i <= quantidadeNumeros; i++)
+                {
+                    Cartas.Add(new Carta(naipe, i));
+                }
+            }
+
+            Embaralhar();
+        }
+
+        public List<Carta> PuxarCartas(int nCartas)
+        {
+            List<Carta> cartasPuxadas = Cartas.TakeLast(nCartas).ToList();
+
+            Cartas.RemoveRange(Cartas.Count - cartasPuxadas.Count, cartasPuxadas.Count);
+            CartasRemovidas.AddRange(cartasPuxadas);
+
+            return cartasPuxadas;
+        }
+
+        public List<Carta> OlharCartasDoTopo(int nCartas)
+        {
+            List<Carta> cartasPuxadas = Cartas.TakeLast(nCartas).ToList();
+
+            return cartasPuxadas;
+        }
+
+        public void Embaralhar()
+        {
+            Random rand = new Random();
+            int n = Cartas.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rand.Next(n + 1);
+                Carta carta = Cartas[k];
+                Cartas[k] = Cartas[n];
+                Cartas[n] = carta;
+            }
+        }
+    }
+}
---
+++ Carta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Naipe
+{
+    Ouro,
+    Espada,
+    Copa,
+    Paus
+}
+
+namespace CardGame
+{
+    public class Carta
+    {
+        public Naipe Naipe;
+        public int Numero;
+
+        public Carta(Naipe naipe, int numero)
+        {
+            if (numero < 1 || numero > 13)
+            {
+                throw new ArgumentException("Número de carta inválido");
+            }
+
+            Naipe = naipe;
+            Numero = numero;
+        }
+
+        public static Carta GerarCartaAleatoria()
+        {
+            Random rand = new Random();
+            Naipe naipe = Enum.GetValues<Naipe>()[rand.Next(4)];
+            int numero = rand.Next(13 + 1);
+
+            return new Carta(naipe, numero);
+        }
+    }
+}
